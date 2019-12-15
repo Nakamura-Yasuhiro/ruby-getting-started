058f6b1d5a10a3604d2ede1cac1703def0a0f043
Your Name <you@example.com>
2019-12-15 03:34:06 +0000

    fix

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -51,46 +51,6 @@ class LinebotController < ApplicationController
                     }
                 end
                 
-                if event.message['text'].eql?('アンケート')
-            # private内のtemplateメソッドを呼び出します。
-            client.reply_message(event['replyToken'], template)
-                end
-       }
-
-    head :ok
-  end
-  
-  
-          private
-
-  def template
-    {
-      "type": "template",
-      "altText": "this is a confirm template",
-      "template": {
-          "type": "confirm",
-          "text": "今日のもくもく会は楽しいですか？",
-          "actions": [
-              {
-                "type": "message",
-                # Botから送られてきたメッセージに表示される文字列です。
-                "label": "楽しい",
-                # ボタンを押した時にBotに送られる文字列です。
-                "text": "楽しい"
-              },
-              {
-                "type": "message",
-                "label": "楽しくない",
-                "text": "楽しくない"
-              }
-          ]
-      }
-    }
-  end
-end      
-                
-                
-                
                 client.reply_message(event["replyToken"], message)
                
            when Line::Bot::Event::MessageType::Location
